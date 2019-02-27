661142e61f718531bd1534fd107631e71cea3e52
Mahmoud Elshakoushy <mahmoudelshakoushy@host-41.33.115.38.tedata.net>
2019-02-27 12:47:23 -0800

    lesson 12

--- ShokshChat/New Group/CreateAccountVC.swift
+++ ShokshChat/New Group/CreateAccountVC.swift
@@ -10,21 +10,30 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var passwordTxt: UITextField!
     @IBOutlet weak var userImg: UIImageView!
     
+    // variables
+    
+    var avatarName = "profileDefault"
+    var avatarColor = "[0.5,0.5,0.5,1]"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func creatAccountPressed(_ sender: Any) {
-        guard let email = emailTxt.text , emailTxt.text != "" else {
-            return }
-        guard let password = passwordTxt.text , passwordTxt.text != "" else {
-            return }
+        guard let email = emailTxt.text , emailTxt.text != "" else {return }
+        guard let password = passwordTxt.text , passwordTxt.text != "" else {return }
+        guard let name = usernameTxt.text , usernameTxt.text != "" else {return }
         
         AuthService.instance.registerUser(email: email, password: password) { (success) in
             if success {
                 AuthService.instance.loginUser(email: email, password: password, completion: { (success) in
                     if success {
-                        print("Logged IN", AuthService.instance.authToken)
+                        AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
+                            if success {
+                                print(UserDataService.instance.name , UserDataService.instance.avatarName)
+                             self.performSegue(withIdentifier: UNWIND, sender: nil)
+                            }
+                        })
                     }
                 })
             }
