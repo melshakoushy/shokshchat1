5b2f0e5d5c33ee4f04863d5b027f1bd014b1b24b
Mahmoud Elshakoushy <mahmoudelshakoushy@host-41.33.115.38.tedata.net>
2019-03-14 13:13:24 -0700

    lesson15

--- ShokshChat/New Group/LoginVC.swift
+++ ShokshChat/New Group/LoginVC.swift
@@ -4,8 +4,35 @@ import UIKit
 
 class LoginVC: UIViewController {
     
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+    }
+    
+    @IBAction func loginPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
+        guard let email = usernameTxt.text, usernameTxt.text != "" else {return}
+        guard let password = passwordTxt.text, passwordTxt.text != "" else {return}
+        
+        AuthService.instance.loginUser(email: email, password: password) { (success) in
+            if success {
+                AuthService.instance.findUserByEmail(completion: { (success) in
+                    if success {
+                        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                        self.spinner.isHidden = true
+                        self.spinner.stopAnimating()
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                })
+            }
+            
+        }
     }
     
     @IBAction func closePressed(_ sender: Any) {
@@ -14,5 +41,11 @@ class LoginVC: UIViewController {
     
     @IBAction func creatAccountPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_Account, sender: nil)
+    }
+    
+    func setupView() {
+        spinner.isHidden = true
+        usernameTxt.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
+        passwordTxt.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedStringKey.foregroundColor: smackPurplePlaceholder])
     }
 }
--- ShokshChat/New Group1/Constants.swift
+++ ShokshChat/New Group1/Constants.swift
@@ -23,8 +23,13 @@ let BASE_URL = "https://chattestshatt.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
+let URL_USER_BY_EMAIL = "\(BASE_URL)user/byEmail/"
 
 // Headers
 let HEADER = [
+    "Contact-Type": "application/json; charset=utf-8"
+]
+let BEARER_HEADER = [
+    "Authorization": "Bearer \(AuthService.instance.authToken)",
     "Contact-Type": "application/json; charset=utf-8"
 ]
